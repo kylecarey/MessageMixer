c6e4e667464039d0c509049cceb54878485ca63a
Kyle Carey <kylec123@outlook.ie>
2021-07-15 22:42:22 +0100

    Created a basic, but functional message mixer. Arrays can be of any length due to how the generateRandomNumber() method works.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// Testfd
+// Message Mixer App
+
+// Random number generator
+const generateRandomNumber = (arrayLength) => {
+    return Math.floor(Math.random() * arrayLength);
+}
+
+// Creating object for storing all the different message parts
+const mixedMessage = {
+    start: ["You","Y'all","Yiz"],
+    middle: ["look","seem","smell","talk"],
+    end: ["tough", "well", "bad"],
+    generateSentence(){
+        const s = this.start[generateRandomNumber(this.start.length)];
+        const m = this.middle[generateRandomNumber(this.middle.length)];
+        const e = this.end[generateRandomNumber(this.end.length)];
+
+        return `${s} ${m} ${e}`;
+    }
+}
+
+const test = mixedMessage;
+console.log(test.generateSentence());
